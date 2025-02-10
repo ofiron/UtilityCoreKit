82ff051e802d771290915dcaa0739717fce90bee
ofiron <ofir@bdapps.net>
2025-02-10 17:08:05 +0200

    Update package platforms iOS 18, macOS 15 & Swift tools 6.0

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "UtilityCoreKit",
-    platforms: [.iOS(.v14), .macOS(.v11)],
+    platforms: [.iOS(.v18), .macOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
